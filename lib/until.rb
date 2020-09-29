cc4c3f7d895047751b620f56a680b12f2666539d
Marc Steele <marcsteele2014@gmail.com>
2020-09-29 19:08:35 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,5 +1,7 @@
-def using_until levitation_force = 10
-  levitation_force = 6
-  levitation_force += 1
-  puts "Wingardium Leviosa"
+def using_until 
+  until levitation_force = 10
+    levitation_force = 6
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+  end
 end
